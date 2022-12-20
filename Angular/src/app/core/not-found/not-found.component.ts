d8c9b4807fcc816f83a5155aed5dd1803d83d006
petrov8 <a.petrov08@outlook.com>
2022-12-20 09:30:49 +0100

    page doens't exist

--- Angular/src/app/core/not-found/not-found.component.ts
+++ Angular/src/app/core/not-found/not-found.component.ts
@@ -1,14 +1,16 @@
+import { urlPaths } from 'src/app/support/url.paths';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['../../jobs/delete/delete.component.css']
 })
 
 export class NotFoundComponent implements OnInit {
+
+  paths = urlPaths
   
   page_not_found: boolean = false
 
